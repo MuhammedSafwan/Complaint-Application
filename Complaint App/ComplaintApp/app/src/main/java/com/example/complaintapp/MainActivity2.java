f71a7d62763a19fd177ce4e69f43be91bf2be14f
MuhammedSafwan <95960097+MuhammedSafwan@users.noreply.github.com>
2023-03-30 23:14:48 +0530

    Add files via upload

---
+++ Complaint App/ComplaintApp/app/src/main/java/com/example/complaintapp/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.complaintapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+
+b1=findViewById(R.id.button);
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+         Intent i = new Intent(MainActivity2.this,LoginActivity.class);
+         startActivity(i);
+
+    }
+});
+
+
+}
+}
---
+++ Complaint App/ComplaintApp/app/src/test/java/com/example/complaintapp/spinner.java
@@ -0,0 +1,4 @@
+package com.example.complaintapp;
+
+public class spinner {
+}
